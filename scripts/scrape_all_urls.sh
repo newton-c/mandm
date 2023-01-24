9d022546112a0f45b3773bcf8f9766f7083f5c6e
newton-c <newton.christop@gmail.com>
2023-01-24 08:28:53 -0500

    writing nested loop to scrape urls from each site

--- scripts/scrape_all_urls.sh
+++ scripts/scrape_all_urls.sh
@@ -1,13 +1,19 @@
 #!/bin/bash
 
+#outer = 1
 for i in $(cat websites.txt)
 do
+	echo $i
 	mkdir ~/Desktop/dissertation/media_and_murder/data/articles/$i/
-	counter = 1
 
-	for j in $(cat $i.txt)
+    #counter = 1
+    n=1
+	for j in $(cat $i".txt")
 	do
-		curl $i > ~/Desktop/dissertation/media_and_murder/data/articles/elcolombiano/$i_$counter
-		counter = $((counter+1))
+		curl $j > ~/Desktop/dissertation/media_and_murder/data/articles/$i/$i"_"$n".html"
+		#curl $j > ~/Desktop/dissertation/media_and_murder/data/articles/elcolombiano/$i_$j
+		#inner = $((inner+1))
+		#counter = $((counter+1))
+		((n++))
 	done
 done
