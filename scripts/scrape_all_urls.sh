0bde3a072bff8139246716cc9de20f4b1cf7c7bb
newton-c <newton.christop@gmail.com>
2023-01-27 22:23:03 -0500

    "changing shell script to run on different OSs"

--- scripts/scrape_all_urls.sh
+++ scripts/scrape_all_urls.sh
@@ -1,14 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 for i in $(cat websites.txt)
 do
 	echo $i
-	mkdir ~/Desktop/dissertation/media_and_murder/data/articles/$i/
+	mkdir -p /Users/cn/Desktop/Dissertation/mandm/data/articles/"$i"
+
 
     n=1
 	for j in $(cat $i".txt")
 	do
-		curl $j > ~/Desktop/dissertation/media_and_murder/data/articles/$i/$i"_"$n".html"
+		curl $j > /Users/cn/Desktop/Dissertation/mandm/data/articles/$i/$i"_"$n".html"
+
 		((n++))
 	done
 done
