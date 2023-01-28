b144a5320387e8b2669c0b02c173f5bda8c34e12
newton-c <newton.christop@gmail.com>
2023-01-27 20:10:15 -0500

    updating sites

--- scripts/scrape_all_urls.sh
+++ scripts/scrape_all_urls.sh
@@ -1,19 +1,14 @@
 #!/bin/bash
 
-#outer = 1
 for i in $(cat websites.txt)
 do
 	echo $i
 	mkdir ~/Desktop/dissertation/media_and_murder/data/articles/$i/
 
-    #counter = 1
     n=1
 	for j in $(cat $i".txt")
 	do
 		curl $j > ~/Desktop/dissertation/media_and_murder/data/articles/$i/$i"_"$n".html"
-		#curl $j > ~/Desktop/dissertation/media_and_murder/data/articles/elcolombiano/$i_$j
-		#inner = $((inner+1))
-		#counter = $((counter+1))
 		((n++))
 	done
 done
