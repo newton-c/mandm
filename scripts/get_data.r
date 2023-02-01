e94012ede39293259e04a0c42b88845d1faeac85
newton-c <newton.christop@gmail.com>
2023-02-01 09:26:01 -0500

    adding el_tiempo

--- scripts/get_data.r
+++ scripts/get_data.r
@@ -3,4 +3,14 @@ library(readr)
 
 setwd("/Users/cn/Desktop/Dissertation/mandm/")
 
-read_csv('data/el_país.csv') %>% head %>% print
+read_csv('data/all_articles.csv') %>% head %>% print
+
+dat <- read_csv('data/all_articles.csv')
+Encoding(dat$article) <- "latin1"
+
+library(syuzhet)
+
+test_sent <- dat$article[1]
+print(test_sent)
+
+get_sentiment(test_sent, method = "nrc", language = "spanish")
